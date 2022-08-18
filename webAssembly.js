39fec59dae310192319e0eada94e4149ec76c376
gabmart1995 <gabmart1995@gmail.com>
2022-08-18 09:57:45 -0400

    wip

--- webAssembly.js
+++ webAssembly.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    loadWasm('/wasm/hash-module/hash.wasm', ( wasm ) => {
+    loadWasm('/wasm/hash-module/hash-module.wasm', ( wasm ) => {
         if ( wasm ) {
             const { go, instance } = wasm;
             go.run( instance );
