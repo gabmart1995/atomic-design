495a3bca7e5e163df6b459fa2dd2f1129e2623e1
gabmart1995 <gabmart1995@gmail.com>
2022-09-01 08:14:51 -0400

    wip

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -16,21 +16,12 @@ class Navbar extends HTMLElement {
             .catch( error => console.error( error ) );
     }
 
-    handleNavbar( $event ) {
-
+    handleNavbar() {
         const element = this.shadowDom.querySelector('ul');
-        element.style.maxHeight = element.style.maxHeight ? null : ( element.scrollHeight + 'px' );
-        // console.log( element.classList.contains('open-menu') );
 
-        /*if ( element.classList.contains('open-menu') ) {
-            element.classList.replace('open-menu', 'close-menu');
-
-            return;
+        if ( element ) {
+            element.style.maxHeight = element.style.maxHeight ? null : ( element.scrollHeight + 'px' );
         }
-        
-        if ( element.classList.contains('close-menu') ) {
-            element.classList.replace('close-menu', 'open-menu');
-        }*/
     }
 
     render() {
